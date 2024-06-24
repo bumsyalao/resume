a03fefd4fe17728c4532572e92f9f03d692ce576
Bunmi Alao <alaobunmi93@gmail.com>
2024-06-24 17:07:37 +0400

    changes 3

--- src/components/awards.jsx
+++ src/components/awards.jsx
@@ -15,30 +15,34 @@ const Awards = () => {
 				</article>
 				<article className="flex-item">
 					<h3>
-					Member of the University of Sheffield Environmental Science Society
+					Foundations: Data, Data, Everywhere
+					<small className="date">2023</small>
 					</h3>
+					<p><a href="https://www.coursera.org/account/accomplishments/verify/L2CXMFUM7KNQ?utm_source=ios&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course" target="_blank">View Certificate</a></p>
 				</article>
+
 
 			</div>
 			<div className="flex">
+			<article className="flex-item">
+					<h3>
+					Member of the University of Sheffield Environmental Science Society
+					</h3>
+				</article>
 				<article className="flex-item">
 					<h3>
 					Member of Afro Caribbean Society					</h3>
 				</article>
-				<article className="flex-item">
+
+			</div>
+			<div className="flex">
+			<article className="flex-item">
 					<h3>
 					Member of the University of Sheffield Environmental Science Society Football team					</h3>
 				</article>
-			</div>
-			<div className="flex">
 				<article className="flex-item">
 					<h3>
 					Member of Politics Society Football team					</h3>
-				</article>
-				<article className="flex-item">
-				<h3>
-				Member of Afro Caribbean Society Football team.
-				</h3>
 				</article>
 
 
