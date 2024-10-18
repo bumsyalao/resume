cd0f8809dde045dad72eab36cb0b434d9ce11145
Bunmi Alao <alaobunmi93@gmail.com>
2024-10-18 08:22:25 +0400

    Add hult mba in education

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -5,15 +5,22 @@ const Education = () => {
 	return (
 		<section className="section education">
 			<h2>Education</h2>
-			<div className="flex" />
-			<article className="flex-item">
-				<h3>
-					Covenant University <small className="date">2010 - 2014</small>
-				</h3>
-				<p>Bachelor's Degree, Computer Science</p>
-				<p> CGPA: 4.43/5 </p>
-				<p>Final year project: A Computing Platform for Metabolic Network Visualization and Topological Analysis.</p>
-			</article>
+			<div className="flex">
+				<article className="flex-item">
+					<h3>
+						Hult International Business School <small className="date">2024-2025</small>
+					</h3>
+					<p>Masters of Business Administration(MBA)</p>
+				</article>
+				<article className="flex-item">
+					<h3>
+						Covenant University <small className="date">2010 - 2014</small>
+					</h3>
+					<p>Bachelor's Degree, Computer Science</p>
+					<p> CGPA: 4.43/5 </p>
+					<p>Final year project: A Computing Platform for Metabolic Network Visualization and Topological Analysis.</p>
+				</article>
+			</div>
 		</section>
 	);
 };
